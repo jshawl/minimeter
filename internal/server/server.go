539407a45d30a16a7ead0f4ec22e52e1de085b6a
Jesse Shawl <jesse@jesse.sh>
2025-11-27 07:47:26 -0500

    add worker queue (#4)

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/jshawl/minimeter/internal/db"
 )
 
 func HandleGetApiMetrics(w http.ResponseWriter, r *http.Request) {
@@ -11,10 +14,46 @@ func HandleGetApiMetrics(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "{\"ok\": 1}")
 }
 
+func HandlePostApiMeasure(jobs chan<- db.Measurement) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if r.Method != http.MethodPost {
+			fmt.Fprint(w, `{"error": "method not allowed"}`)
+			return
+		}
+		var measurement db.Measurement
+
+		err := json.NewDecoder(r.Body).Decode(&measurement)
+
+		if err != nil {
+			log.Println(err)
+			fmt.Fprint(w, `{"ok":1}`)
+			return
+		}
+
+		select {
+		case jobs <- measurement:
+			// queued successfully
+		default:
+			// channel full
+			log.Println("metric dropped: queue full")
+		}
+		fmt.Fprint(w, `{"ok":1}`)
+	}
+}
+
 func NewServer() http.Handler {
 	mux := http.NewServeMux()
 
+	jobs := make(chan db.Measurement, 50_000)
+
+	db, err := db.NewDB("minimeter.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.StartMeasurementWorker(jobs)
 	mux.HandleFunc("/api/metrics", HandleGetApiMetrics)
+	mux.HandleFunc("/api/measure", HandlePostApiMeasure(jobs))
 
 	return mux
 }
